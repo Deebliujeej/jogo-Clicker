4efb998f63855c031f288f2205d35d783e9a1b21
deebliujeej <gabriel.zingelerski@unifebe.edu.br>
2025-06-24 00:30:42 -0300

    jogo clicker

---
+++ script.js
@@ -0,0 +1,56 @@
+
+let pontos = 0;
+let pontosPorClique = 1;
+let precoupgrade1 = 10;
+let precoupgrade2 = 50;
+let precoupgrade3 = 1000;
+
+const pontosDiv = document.getElementById('points');
+const clickBtn = document.getElementById('click-btn');
+const valorupgrade1 = document.getElementById('valorupgrade1');
+const valorupgrade2 = document.getElementById('valorupgrade2');
+const valorupgrade3 = document.getElementById('valorupgrade3');
+
+
+
+clickBtn.onclick = () => {
+    pontos += pontosPorClique;
+    atualizarPontos();
+};
+
+function atualizarPontos() {
+    pontosDiv.textContent = "Pontos: " + (pontos);
+}
+
+// Upgrade 1
+document.getElementById("upgrade1").onclick = () => {
+    if (pontos >= precoupgrade1) {
+        pontos -= precoupgrade1;
+        pontosPorClique += 1;
+        precoupgrade1 = precoupgrade1 * 2;
+        valorupgrade1.innerText = precoupgrade1 + ' pontos';
+        atualizarPontos();
+    }
+};
+
+// Upgrade 2
+document.getElementById("upgrade2").onclick = () => {
+    if (pontos >= precoupgrade2) {
+        pontos -= precoupgrade2;
+        pontosPorClique *= 2;
+        precoupgrade2 = precoupgrade2 * 4;
+        valorupgrade2.innerText = precoupgrade2 + ' pontos';
+        atualizarPontos();
+    }
+};
+
+// Upgrade 3 - multiplicador
+document.getElementById("upgrade3").onclick = () => {
+    if (pontos >= precoupgrade3) {
+        pontos -= precoupgrade3;
+        pontosPorClique *= 3;
+        precoupgrade3 = precoupgrade3 * 8;
+        valorupgrade3.innerText = precoupgrade3 + ' pontos';
+        atualizarPontos();
+    }
+};
